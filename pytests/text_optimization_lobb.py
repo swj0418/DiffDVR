d57e72f9eef3db6de9b6b7f57f396317b39e6718
sangwon <swj0418@hotmail.com>
2024-03-26 06:14:43 -0500

    new data

--- pytests/text_optimization_lobb.py
+++ pytests/text_optimization_lobb.py
@@ -131,12 +131,20 @@ if __name__ == '__main__':
 
     print("Create forward difference settings")
     differences_settings = pyrenderer.ForwardDifferencesSettings()
-    differences_settings.D = 4 * 3  # I want gradients for all inner control points
+    # differences_settings.D = 4 * 3  # I want gradients for all inner control points
+    # derivative_tf_indices = torch.tensor([[
+    #     [-1, -1, -1, -1, -1],
+    #     [0, 1, 2, 3, -1],
+    #     [4, 5, 6, 7, -1],
+    #     [8, 9, 10, 11, -1],
+    #     [-1, -1, -1, -1, -1]
+    # ]], dtype=torch.int32)
+    differences_settings.D = 3  # I want gradients for all inner control points
     derivative_tf_indices = torch.tensor([[
         [-1, -1, -1, -1, -1],
-        [0, 1, 2, 3, -1],
-        [4, 5, 6, 7, -1],
-        [8, 9, 10, 11, -1],
+        [-1, -1, -1, 0, -1],
+        [-1, -1, -1, 1, -1],
+        [-1, -1, -1, 2, -1],
         [-1, -1, -1, -1, -1]
     ]], dtype=torch.int32)
     differences_settings.d_tf = derivative_tf_indices.to(device=device)
@@ -177,12 +185,20 @@ if __name__ == '__main__':
     #     [0.5, 0.6, 0.4, 5, 0.55],
     #     [0.9, 0.99, 0.99, 0.001, 1]
     # ]], dtype=dtype, device=device)
+    # initial_tf = torch.tensor([[
+    #     # r,g,b,a,pos
+    #     [0.9, 0.01, 0.01, 0.001, 0],
+    #     [0.9, 0.58, 0.46, 0.001, 0.45],
+    #     [0.9, 0.61, 0.50, 0.8 * opacity_scaling, 0.5],
+    #     [0.9, 0.66, 0.55, 0.001, 0.55],
+    #     [0.9, 0.99, 0.99, 0.001, 1]
+    # ]], dtype=dtype, device=device)
     initial_tf = torch.tensor([[
         # r,g,b,a,pos
         [0.9, 0.01, 0.01, 0.001, 0],
-        [0.9, 0.58, 0.46, 0.001, 0.45],
+        [0.9, 0.58, 0.46, 4, 0.45],
         [0.9, 0.61, 0.50, 0.8 * opacity_scaling, 0.5],
-        [0.9, 0.66, 0.55, 0.001, 0.55],
+        [0.9, 0.66, 0.55, 7, 0.55],
         [0.9, 0.99, 0.99, 0.001, 1]
     ]], dtype=dtype, device=device)
     print("Initial tf (original):", initial_tf)
