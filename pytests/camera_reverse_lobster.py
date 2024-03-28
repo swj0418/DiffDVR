bbdaa80cba3043d96af584798204e0e882420b41
sangwon <swj0418@hotmail.com>
2024-03-28 09:16:09 -0500

    random TF for lobsters

--- pytests/camera_reverse_lobster.py
+++ pytests/camera_reverse_lobster.py
@@ -295,7 +295,7 @@ if __name__ == '__main__':
         # ray_start, ray_dir = pyrenderer.Camera.generate_rays(viewport, fov_radians, W, H)
         # inputs.camera = pyrenderer.CameraPerPixelRays(ray_start, ray_dir)
 
-        inputs.tf = torch.randn(size=(1, 1, 8, 5), dtype=dtype, device=device)
+        current_tf = torch.randn(size=(1, 1, 8, 5), dtype=dtype, device=device).requires_grad_()
 
         loss, transformed_tf, color = model(current_tf)
         print(transformed_tf)
