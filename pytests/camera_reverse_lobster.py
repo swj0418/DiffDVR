23f3c105dacbd2cfe18aacb1e0d18e13d67ef998
sangwon <swj0418@hotmail.com>
2024-03-28 09:55:12 -0500

    very simple TF for lobsters

--- pytests/camera_reverse_lobster.py
+++ pytests/camera_reverse_lobster.py
@@ -225,7 +225,7 @@ if __name__ == '__main__':
         # r,g,b,a,pos
         [0.23, 0.30, 0.75, 0.0 * opacity_scaling, 0.01 / 255],
         [0.96, 0.75, 0.65, 0.0 * opacity_scaling, 120 / 255],
-        [0.70, 0.015, 0.15, 0.9 * opacity_scaling, (255 * 3) / 255]
+        [0.70, 0.015, 0.15, 0.9 * opacity_scaling, 255]
     ]], dtype=dtype, device=device)
 
     print("Initial tf (original):", initial_tf)
