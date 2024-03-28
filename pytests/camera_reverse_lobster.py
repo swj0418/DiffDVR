615978646713f5169354f79593d6f338491fd854
sangwon <swj0418@hotmail.com>
2024-03-28 10:26:27 -0500

    better figure

--- pytests/camera_reverse_lobster.py
+++ pytests/camera_reverse_lobster.py
@@ -350,7 +350,7 @@ if __name__ == '__main__':
         print("Iteration % 4d, Loss: %7.5f, Cosine Distance: %7.5f" % (iteration, loss.item(), score.item()))
 
     print("Visualize Optimization")
-    fig, axs = plt.subplots(5, 2, figsize=(8, 6))
+    fig, axs = plt.subplots(5, 2, figsize=(12, 6))
     axs[0, 0].imshow(reference_color_image[:, :, 0:3])
     tfvis.renderTfLinear(reference_tf, axs[0, 1])
     axs[1, 0].imshow(reconstructed_color[0])
@@ -371,7 +371,7 @@ if __name__ == '__main__':
     axs[3, 1].plot(reconstructed_loss)
     axs[4, 1].plot(reconstructed_cliploss)
 
-    for i in range(4):
+    for i in range(3):
         for j in range(2):
             axs[i, j].set_xticks([])
             if j == 0: axs[i, j].set_yticks([])
