3719c83773a03c40f9f2d861b998ce9b8813d697
sangwon <swj0418@hotmail.com>
2024-03-28 09:27:56 -0500

    random TF for lobsters

--- pytests/camera_reverse_lobster.py
+++ pytests/camera_reverse_lobster.py
@@ -297,9 +297,9 @@ if __name__ == '__main__':
 
         current_tf = torch.rand(size=(1, 8, 5), dtype=dtype, device=device)
         current_tf.requires_grad_()
-        # current_tf = current_tf[:, :, 4]
+        current_tf = current_tf[:, :, 3] * opacity_scaling
         for i in range(8):
-            current_tf[:, i, 5] = i / 10
+            current_tf[:, i, 4] = i / 10
 
         loss, transformed_tf, color = model(current_tf)
         print(transformed_tf)
