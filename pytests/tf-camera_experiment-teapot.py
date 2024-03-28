e97ab07e5fa1e16a52e6819cc37fa46232ca15fa
sangwon <swj0418@hotmail.com>
2024-03-28 15:06:35 -0500

    teapot

--- pytests/tf-camera_experiment-teapot.py
+++ pytests/tf-camera_experiment-teapot.py
@@ -30,8 +30,8 @@ clipmodel = clipmodel.cuda()
 text = tokenizer(["A CT scan of a teapot"]).cuda()
 
 torch.set_printoptions(sci_mode=False, precision=3)
-lr = 0.05
-step_size = 400
+lr = 0.5
+step_size = 200
 gamma = 0.1
 iterations = 400  # Optimization iterations
 B = 1  # batch dimension
