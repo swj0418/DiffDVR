924318e3a0aa1cbb5f0f82106bf4279cba572b50
sangwon <swj0418@hotmail.com>
2024-03-29 11:41:39 -0500

    parameterization

--- pytests/parameterization.py
+++ pytests/parameterization.py
@@ -158,7 +158,7 @@ class TransformTFParameterization(torch.nn.Module):
         start = self._check_start_condition(param_tf[0])
         width = self._check_width_condition(param_tf[0], param_tf[1])
         height = self._check_height_condition(param_tf[2])
-        tf = self._build_tf(start, width, height)
+        tf = self._build_tf(tf, start, width, height)
 
         return torch.cat([
             self.sigmoid(tf[:, :, 0:3]),  # color
