2dcc1761ea89a9ae5c47a0974284165f6e8c3cf3
sangwon <swj0418@hotmail.com>
2024-03-29 12:56:37 -0500

    parameterization

--- pytests/parameterization.py
+++ pytests/parameterization.py
@@ -379,6 +379,7 @@ if __name__ == '__main__':
         # preprocess and embed
         # Tensor [C, H, W]
         tmpimg = color[:, :, :, :3][0]
+        print(tmpimg.shape)
 
         prep_img = grad_preprocess(tmpimg)
         prep_img = prep_img.float()
