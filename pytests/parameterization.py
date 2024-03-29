4404ecd5661bd4d05ad0318be8377f24ef253359
sangwon <swj0418@hotmail.com>
2024-03-29 11:42:18 -0500

    parameterization

--- pytests/parameterization.py
+++ pytests/parameterization.py
@@ -156,7 +156,7 @@ class TransformTFParameterization(torch.nn.Module):
 
         # Opacity, Control Point
         start = self._check_start_condition(param_tf[0])
-        width = self._check_width_condition(param_tf[0], param_tf[1])
+        start, width = self._check_width_condition(param_tf[0], param_tf[1])
         height = self._check_height_condition(param_tf[2])
         tf = self._build_tf(tf, start, width, height)
 
