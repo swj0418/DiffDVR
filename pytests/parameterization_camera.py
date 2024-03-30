f4f69654fb60a3c59fa8c2d6e992dbaea92149d2
sangwon <swj0418@hotmail.com>
2024-03-30 15:32:16 -0500

    parameterization

--- pytests/parameterization_camera.py
+++ pytests/parameterization_camera.py
@@ -214,7 +214,7 @@ if __name__ == '__main__':
 
     print("Create forward difference settings")
     differences_settings = pyrenderer.ForwardDifferencesSettings()
-    differences_settings.D = 15  # TF + camera
+    differences_settings.D = 15 + 6  # TF + camera
     # derivative_tf_indices = torch.tensor([[[0, 1, 2, 3, 4, 5]]], dtype=torch.int32)
     derivative_tf_indices = torch.tensor([[
         [0, 1, 2, 3, 4],
@@ -268,12 +268,12 @@ if __name__ == '__main__':
             # print(c_gradients.shape, gradients.shape)
 
             # Map to output variables
-            # grad_ray_start = c_gradients[..., 15:18]
-            # grad_ray_dir = c_gradients[..., 18:21]
+            grad_ray_start = c_gradients[..., 15:18]
+            grad_ray_dir = c_gradients[..., 18:21]
             # print(grad_ray_dir.sum(), grad_ray_start.sum())
 
-            grad_ray_start = c_gradients[..., 0:15]
-            grad_ray_dir = c_gradients[..., 0:15]
+            # grad_ray_start = c_gradients[..., 0:3]
+            # grad_ray_dir = c_gradients[..., 3:6]
 
             # TF map
             grad_tf = torch.zeros_like(transformed_tf)
