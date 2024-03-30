66d9808bd9ac4c98e6f8bc41137e247f6e28086d
sangwon <swj0418@hotmail.com>
2024-03-30 10:24:03 -0500

    parameterization

--- pytests/parameterization.py
+++ pytests/parameterization.py
@@ -27,7 +27,7 @@ tokenizer = open_clip.get_tokenizer('ViT-B-32')
 # clipmodel, _, preprocess = open_clip.create_model_and_transforms('ViT-g-14', pretrained='laion2b_s34b_b88k')
 grad_preprocess = _clip_preprocess(224)
 clipmodel = clipmodel.cuda()
-text = tokenizer(["A CT scan of a teapot"]).cuda()
+text = tokenizer(["A CT scan of a blue teapot"]).cuda()
 
 torch.set_printoptions(sci_mode=False, precision=3)
 lr = 2.0
