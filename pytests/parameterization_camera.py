5154566fced0df3e394f2bb48a54dd1460b1de44
sangwon <swj0418@hotmail.com>
2024-04-01 09:41:17 -0500

    parameterization

--- pytests/parameterization_camera.py
+++ pytests/parameterization_camera.py
@@ -184,7 +184,7 @@ if __name__ == '__main__':
     # [0, 2pi]
     camera_initial_pitch = torch.tensor([[np.radians(45)]], dtype=dtype,
                                         device=device)  # torch.tensor([[np.radians(-14.5)]], dtype=dtype, device=device)
-    camera_initial_yaw = torch.tensor([[np.radians(120)]], dtype=dtype,
+    camera_initial_yaw = torch.tensor([[np.radians(160)]], dtype=dtype,
                                       device=device)  # torch.tensor([[np.radians(113.5)]], dtype=dtype, device=device)
     camera_initial_distance = torch.tensor([[2.0]], dtype=dtype, device=device)
 
