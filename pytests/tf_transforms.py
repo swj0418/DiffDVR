e2934d1a958ef166cde582a19afbd522a2e97b68
sangwon <swj0418@hotmail.com>
2024-04-01 10:28:14 -0500

    parameterization

--- pytests/tf_transforms.py
+++ pytests/tf_transforms.py
@@ -1,43 +1,41 @@
 import torch
 
-from pytests.nonparam_camera_ import dtype, device
-
-
 class TransformTFParameterization(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, dtype, device):
         super().__init__()
         self.sigmoid = torch.nn.Sigmoid()
         self.softplus = torch.nn.Softplus()
         self.cp_min, self.cp_max = 0, 255
+        self.dtype, self.device = dtype, device
 
     def _check_start_condition(self, start):
         # 0 < starting point < 254
         if start < 0:
-            start = torch.tensor([0], dtype=dtype, device=device)
+            start = torch.tensor([0], dtype=self.dtype, device=self.device)
         elif start > 254:
-            start = torch.tensor([254], dtype=dtype, device=device)
+            start = torch.tensor([254], dtype=self.dtype, device=self.device)
         return start
 
     def _check_width_condition(self, start, width, fix_width=True):
         # start + width < 255
         if start + width > 255:
             if fix_width:
-                width = torch.tensor([(start + width) - 255], dtype=dtype, device=device)
+                width = torch.tensor([(start + width) - 255], dtype=self.dtype, device=self.device)
             else:
-                start = torch.tensor([(start + width) - 255], dtype=dtype, device=device)
+                start = torch.tensor([(start + width) - 255], dtype=self.dtype, device=self.device)
         return start, width
 
     def _check_height_condition(self, height):
         # height < 100 (?)
         if height > 100:
-            height = torch.tensor([100], dtype=dtype, device=device)
+            height = torch.tensor([100], dtype=self.dtype, device=self.device)
         if height < 0:
-            height = torch.tensor([0], dtype=dtype, device=device)
+            height = torch.tensor([0], dtype=self.dtype, device=self.device)
         return height
 
     def _build_tf(self, start, width, height, rgb):
         # Convert LAB into RGB
-        tf = torch.zeros(size=(1, 5, 5), dtype=dtype, device=device)
+        tf = torch.zeros(size=(1, 5, 5), dtype=self.dtype, device=self.device)
         tf[:, 1, 4] = start
         tf[:, 2, 4] = start + (width / 2.)
         tf[:, 2, 3] = height
@@ -69,8 +67,9 @@ class TransformTFParameterization(torch.nn.Module):
 
 
 class InverseTransformTFParameterization(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, dtype, device):
         super().__init__()
+        self.dtype, self.device = dtype, device
 
     def forward(self, tf):
         # Tf -> start, width, height
@@ -94,7 +93,7 @@ class InverseTransformTFParameterization(torch.nn.Module):
         width = tf[:, 2, 4] - start
         height = tf[:, 1: 3]
         r, g, b = tf[:, 1, 0], tf[:, 1, 1], tf[:, 1, 2]
-        return torch.tensor([start, width, height, r, g, b], dtype=dtype, device=device)
+        return torch.tensor([start, width, height, r, g, b], dtype=self.dtype, device=self.device)
 
 
 class InverseTransformCamera(torch.nn.Module):
