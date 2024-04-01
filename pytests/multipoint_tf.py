5b77f1b1dbeb14f276af53244c0f276dade14baa
Sangwon Jeong <swj0418@hotmail.com>
2024-04-01 13:32:20 -0500

    code improvement

--- pytests/multipoint_tf.py
+++ pytests/multipoint_tf.py
@@ -27,9 +27,9 @@ tokenizer = open_clip.get_tokenizer('ViT-B-32')
 # clipmodel, _, preprocess = open_clip.create_model_and_transforms('ViT-g-14', pretrained='laion2b_s34b_b88k')
 grad_preprocess = _clip_preprocess(224)
 clipmodel = clipmodel.cuda()
-text = tokenizer(["A teapot"]).cuda()
+text = tokenizer(["A tree"]).cuda()
 
-dataset = VolumeDatasetLoader('teapot')
+dataset = VolumeDatasetLoader('tree')
 volume_dataset = ov.load_dataset(dataset.get_url(), cache_dir='./cache')
 data = volume_dataset.read(x=(0, dataset.get_xyz()[0]), y=(0, dataset.get_xyz()[1]), z=(0, dataset.get_xyz()[2]))
 
