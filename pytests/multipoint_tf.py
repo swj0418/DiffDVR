df575a62157c1aac202559d33996635b9e0f5ddf
sangwon <swj0418@hotmail.com>
2024-04-03 11:21:08 -0500

    code cleanup

--- pytests/multipoint_tf.py
+++ pytests/multipoint_tf.py
@@ -52,7 +52,7 @@ torch.set_printoptions(sci_mode=False, precision=3)
 lr = 2.0
 step_size = 200
 gamma = 0.1
-lamb = 0.02
+lamb = 0.05
 iterations = 600  # Optimization iterations
 B = 1  # batch dimension
 H = 224  # screen height
