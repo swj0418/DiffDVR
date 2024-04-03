5d6c021c2bf181e180f50a9f9e102d7a0b925052
Sangwon Jeong <swj0418@hotmail.com>
2024-04-03 15:34:28 -0500

    experiment script

--- pytests/run_experiment.sh
+++ pytests/run_experiment.sh
@@ -1,33 +1,68 @@
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 0 --yaw 0 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 30 --yaw 0 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 60 --yaw 0 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 89 --yaw 0 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 0 --yaw 30 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 30 --yaw 30 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 60 --yaw 30 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 89 --yaw 30 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 0 --yaw 60 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 30 --yaw 60 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 60 --yaw 60 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 89 --yaw 60 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 0 --yaw 89 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 30 --yaw 89 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 60 --yaw 89 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 89 --yaw 89 --seed 0
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a teapot" --pitch 0 --yaw 0 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a teapot" --pitch 30 --yaw 0 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a teapot" --pitch 60 --yaw 0 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a teapot" --pitch 89 --yaw 0 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a teapot" --pitch 0 --yaw 30 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a teapot" --pitch 30 --yaw 30 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a teapot" --pitch 60 --yaw 30 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a teapot" --pitch 89 --yaw 30 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a teapot" --pitch 0 --yaw 60 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a teapot" --pitch 30 --yaw 60 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a teapot" --pitch 60 --yaw 60 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a teapot" --pitch 89 --yaw 60 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a teapot" --pitch 0 --yaw 89 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a teapot" --pitch 30 --yaw 89 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a teapot" --pitch 60 --yaw 89 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a teapot" --pitch 89 --yaw 89 --seed 1
 
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 0 --yaw 0 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 30 --yaw 0 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 60 --yaw 0 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 89 --yaw 0 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 0 --yaw 30 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 30 --yaw 30 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 60 --yaw 30 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 89 --yaw 30 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 0 --yaw 60 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 30 --yaw 60 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 60 --yaw 60 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 89 --yaw 60 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 0 --yaw 89 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 30 --yaw 89 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 60 --yaw 89 --seed 0
-python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 89 --yaw 89 --seed 0
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a red teapot" --pitch 0 --yaw 0 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a red teapot" --pitch 30 --yaw 0 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a red teapot" --pitch 60 --yaw 0 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a red teapot" --pitch 89 --yaw 0 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a red teapot" --pitch 0 --yaw 30 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a red teapot" --pitch 30 --yaw 30 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a red teapot" --pitch 60 --yaw 30 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a red teapot" --pitch 89 --yaw 30 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a red teapot" --pitch 0 --yaw 60 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a red teapot" --pitch 30 --yaw 60 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a red teapot" --pitch 60 --yaw 60 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a red teapot" --pitch 89 --yaw 60 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a red teapot" --pitch 0 --yaw 89 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a red teapot" --pitch 30 --yaw 89 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a red teapot" --pitch 60 --yaw 89 --seed 1
+python multipoint_tf.py --volume teapot --prompt "Volume rendering of a red teapot" --pitch 89 --yaw 89 --seed 1
+
+
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 0 --yaw 0 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 30 --yaw 0 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 60 --yaw 0 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 89 --yaw 0 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 0 --yaw 30 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 30 --yaw 30 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 60 --yaw 30 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 89 --yaw 30 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 0 --yaw 60 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 30 --yaw 60 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 60 --yaw 60 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 89 --yaw 60 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 0 --yaw 89 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 30 --yaw 89 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 60 --yaw 89 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a lobster" --pitch 89 --yaw 89 --seed 1
+
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 0 --yaw 0 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 30 --yaw 0 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 60 --yaw 0 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 89 --yaw 0 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 0 --yaw 30 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 30 --yaw 30 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 60 --yaw 30 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 89 --yaw 30 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 0 --yaw 60 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 30 --yaw 60 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 60 --yaw 60 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 89 --yaw 60 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 0 --yaw 89 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 30 --yaw 89 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 60 --yaw 89 --seed 1
+python multipoint_tf.py --volume lobster --prompt "Volume rendering of a red lobster" --pitch 89 --yaw 89 --seed 1
