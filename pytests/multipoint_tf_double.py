32a46278c173a914f2239a60436ddcfc751f5eb8
sangwon <swj0418@hotmail.com>
2024-04-04 21:09:08 -0500

    double optimizer

--- pytests/multipoint_tf_double.py
+++ pytests/multipoint_tf_double.py
@@ -191,7 +191,8 @@ if __name__ == '__main__':
     current_tf = initial_tf.clone()
     current_tf.requires_grad_()
 
-    optimizer = torch.optim.Adam([current_tf[:, :, 0:3]], lr=lr)
+    optimizer = torch.optim.Adam([current_tf], lr=lr)
+    # optimizer = torch.optim.Adam([current_tf[:, :, 0:3]], lr=lr)
     # optimizer_opacity = torch.optim.Adam([current_tf[:, :, 3:4]], lr=opacity_lr)
     # optimizer = torch.optim.SGD([current_tf], lr=lr, momentum=0.9)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
