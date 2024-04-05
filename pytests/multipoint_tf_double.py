767ef964766850cbf3cb10017742368c74908a40
sangwon <swj0418@hotmail.com>
2024-04-05 09:27:49 -0500

    zero grad...

--- pytests/multipoint_tf_double.py
+++ pytests/multipoint_tf_double.py
@@ -35,8 +35,8 @@ def parse_args():
 args = parse_args()
 
 torch.set_printoptions(sci_mode=False, precision=3)
-lr = 0.01
-opacity_lr = 1.0
+lr = 1.0
+opacity_lr = 2.0
 step_size = 600
 gamma = 0.1
 lamb = 0
