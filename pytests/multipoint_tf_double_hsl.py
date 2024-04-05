2fe8054d55f22041145dad20ba70a2d9a12e9bdb
Sangwon Jeong <swj0418@hotmail.com>
2024-04-05 15:35:07 -0500

    linear annealing

--- pytests/multipoint_tf_double_hsl.py
+++ pytests/multipoint_tf_double_hsl.py
@@ -201,7 +201,7 @@ if __name__ == '__main__':
     # optimizer_opacity = torch.optim.Adam([current_tf_opacity], lr=opacity_lr)
 
     optimizer = torch.optim.SGD([current_tf], lr=lr)
-    optimizer_opacity = torch.optim.SGD([current_tf_opacity], lr=opacity_lr)
+    optimizer_opacity = torch.optim.SGD([current_tf_opacity[0]], lr=opacity_lr)
 
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
     scheduler_opacity = torch.optim.lr_scheduler.StepLR(optimizer_opacity, step_size=step_size, gamma=gamma)
