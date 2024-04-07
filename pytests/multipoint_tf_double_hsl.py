00c19ae003d78f8b51937f7044ddc7629b61705b
Sangwon Jeong <swj0418@hotmail.com>
2024-04-07 11:56:27 -0500

    vit-b

--- pytests/multipoint_tf_double_hsl.py
+++ pytests/multipoint_tf_double_hsl.py
@@ -52,11 +52,11 @@ W = 224 # screen width
 experiment_name = f'{args.volume}_{args.prompt}_{args.pitch}_{args.yaw}_{args.seed}'
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# clipmodel, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
-# tokenizer = open_clip.get_tokenizer('ViT-B-32')
+clipmodel, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
+tokenizer = open_clip.get_tokenizer('ViT-B-32')
 
-clipmodel, _, preprocess = open_clip.create_model_and_transforms('ViT-g-14', pretrained='laion2b_s34b_b88k')
-tokenizer = open_clip.get_tokenizer('ViT-g-14')
+# clipmodel, _, preprocess = open_clip.create_model_and_transforms('ViT-g-14', pretrained='laion2b_s34b_b88k')
+# tokenizer = open_clip.get_tokenizer('ViT-g-14')
 
 grad_preprocess = _clip_preprocess(224)
 
@@ -192,8 +192,8 @@ if __name__ == '__main__':
     reconstructed_cliploss = []
 
     # Working parameters
-    current_tf = initial_tf.clone()[0]
-    current_tf_opacity = initial_tf.clone()[0]
+    current_tf = initial_tf.clone()
+    current_tf_opacity = initial_tf.clone()
     current_tf.requires_grad_()
     current_tf_opacity.requires_grad_()
 
